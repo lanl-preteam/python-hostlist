75487914585454ba1d54fda681d80387c0a6e292
Kent Engström <kent@nsc.liu.se>
2008-09-17 13:46:45 +0200

    Version 1.1
    Move command line utility to a separate 'hostlist file'.
    Add python-hostlist.spec to build RPM packages.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name         = "python-hostlist",
-      version      = "1.0", # Change comment in hostlist.py too!
+      version      = "1.1", # Change in hostlist{,.py}, README, python-hostlist.spec too!
       description  = "Python module for hostlist handling",
       long_description = "The hostlist.py module knows how to expand and collect LLNL hostlist expressions.",
       author       = "Kent Engström",
@@ -11,4 +11,5 @@ setup(name         = "python-hostlist",
       url          = "http://www.nsc.liu.se/~kent/python-hostlist/",
       license      = "GPL2+",
       py_modules   = ["hostlist"],
+      scripts      = ["hostlist"],
       )
